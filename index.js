f5c8839cde2b49547264e81d048277b543d8b60c
savelichalex <savelichalex93@gmail.com>
2015-09-21 12:02:53 +0300

    Change html2hyperscript to html2hyperscript and add SVG support

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ _.templateSettings = {
 };
 
 module.exports = function ( template ) {
-    template = template.replace(/(\{\{[\-=]?)([\s\S]+?)(\}\})/g, function( i, fBr, arg, sBr ) { return fBr + arg.replace( /\s+/g, '' ).replace(/elseif/g, 'else if') + sBr;  } );
-    var hs = convert( template );
-    var tplFn = _.template( hs.replace( /\r?\n|\r|\t+|\s{2,}/g, '' ).replace(/\"SVGAttributeHook\(([^\)]+)\)\"/g, function( i, f ) { return 'SVGAttributeHook(' + f + ')'; }) );
-    return 'module.exports = ' + tplFn.source;
+    var callback = this.async();
+    template = template.replace( /\r?\n+|\r|\t+/g, '' ).replace(/\s{2,}/g, ' ').replace(/(\{\{[\-=]?)([\s\S]+?)(\}\})/g, function( i, fBr, arg, sBr ) { return fBr + arg.replace( /\s+/g, '' ).replace(/elseif/g, 'else if') + sBr;  } );
+    convert( template, function ( err, hs ) {
+        if ( err ) {
+            return callback( err );
+        }
+        var tplFn = _.template( hs.replace( /\r?\n+|\r|\t+|\s{2,}/g, '' ).replace(/\s{2,}/g, ' ').replace(/\"SVGAttributeHook\(([^\)]+)\)\"/g, function( i, f ) { return 'SVGAttributeHook(' + f + ')'; }) );
+        callback( null, 'module.exports = ' + tplFn.source );
+    } )
 };
